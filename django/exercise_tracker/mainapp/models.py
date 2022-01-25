4b10e28f45d6371b89bcb9cf36af78f266942f9c
Guido de Giovanni <guidito.dg86@gmail.com>
2022-01-25 01:01:09 -0300

    Adding users complete

--- django/exercise_tracker/mainapp/models.py
+++ django/exercise_tracker/mainapp/models.py
@@ -1,9 +1,18 @@
+import secrets
 from django.db import models
 
 class User(models.Model):
-    _id = models.AutoField(primary_key=True)
+    _id = models.CharField(max_length=25, primary_key=True)
     username = models.CharField(max_length=100)
 
+    def save(self, *args, **kwargs):
+        if not self._id:
+            already_exists = True
+            while already_exists:
+                _id = secrets.token_hex(12)
+                already_exists = User.objects.filter(pk=_id).exists()
+            self._id = _id
+        super(User, self).save(*args, **kwargs)
 
 class Exercise(models.Model):
     id_user = models.ForeignKey(
--- django/exercise_tracker/mainapp/views.py
+++ django/exercise_tracker/mainapp/views.py
@@ -1,7 +1,21 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import json
+from mainapp.models import User
+import secrets
 
 class CreateUserView(APIView):
     def post(self, request):
-        return Response({
-            "test": request.data })
+        try:          
+            new_username = request.data['username']
+            already_exists = User.objects.filter(username=new_username).exists()
+            if already_exists:
+                return Response({"error": "user already exists"})           
+            new_user_record = User(username=new_username)
+            new_user_record.save()
+            return Response({"username": new_username, "_id":new_user_record._id})               
+                
+        except:
+            return Response({"error": "bad request"})
+
+
